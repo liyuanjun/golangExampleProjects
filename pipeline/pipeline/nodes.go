ef7ecd4c54d2d86dee1f5a3d57f4c03234b33f47
tom <liyuanjunmail@qq.com>
2018-01-21 22:47:42 +0800

    nodes .

--- pipeline/pipeline/nodes.go
+++ pipeline/pipeline/nodes.go
@@ -1,8 +1,15 @@
 // 节点
 package pipeline
 
+import (
+	"sort"
+	"io"
+	"encoding/binary"
+	"math/rand"
+)
+
 // 将Array中的数据导入到channel
-func ArraySource(data ... int) chan int {
+func ArraySource(data ... int) <-chan int {
 	out := make(chan int)
 	// 必须在go routine中使用channel
 	go func() {
@@ -10,6 +17,97 @@ func ArraySource(data ... int) chan int {
 			out <- v
 		}
 		// 关闭channel，表示传输结束
+		close(out)
+	}()
+	return out
+}
+
+// 取出channel数据在内存排序，然后返回排好序的channel
+func InMemorySort(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		// 将channel中的数据读进内存
+		var data []int
+		for v := range in {
+			data = append(data, v)
+		}
+		// 排序
+		sort.Ints(data)
+		// 输出
+		for _, v := range data {
+			out <- v
+		}
+		// 关闭channel
+		close(out)
+	}()
+	return out
+}
+
+// 合并两个channel
+func Merge2(int1, int2 <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		v1, ok1 := <-int1
+		v2, ok2 := <-int2
+		for ok1 || ok2 {
+			// 比较获取最小值，并为其重新设置变量值
+			if !ok2 || (ok1 && v1 <= v2) {
+				out <- v1
+				v1, ok1 = <-int1
+			} else {
+				out <- v2
+				v2, ok2 = <-int2
+			}
+		}
+		// 关闭channel
+		close(out)
+	}()
+	return out
+}
+
+// 读取文件作为数据源
+func ReaderSource(reader io.Reader) <-chan int {
+	out := make(chan int)
+	go func() {
+		// 声明一个8字节大小的buffer对象
+		buffer := make([]byte, 8)
+		for {
+			n, err := reader.Read(buffer)
+			if err != nil { // EOF
+				break
+			}
+			if n > 0 {
+				// 从buff中获取数据
+				v := int(binary.BigEndian.Uint64(buffer))
+				out <- v
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+// 将channel数据写入文件
+func WriterSink(writer io.Writer, in <-chan int) {
+	for {
+		buffer := make([]byte, 8)
+		// 从channel中取数据
+		if data, ok := <-in; ok {
+			binary.BigEndian.PutUint64(buffer, uint64(data))
+			writer.Write(buffer)
+		} else {
+			break
+		}
+	}
+}
+
+// 随机int 数据源
+func RandomIntSource(count int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < count; i++ {
+			out <- rand.Int()
+		}
 		close(out)
 	}()
 	return out
--- pipeline/test/test_pipeline.go
+++ pipeline/test/test_pipeline.go
@@ -3,18 +3,98 @@ package main
 import (
 	"github.com/amlyj/golangExampleProjects/pipeline/pipeline"
 	"fmt"
+	"os"
+	"math/rand"
+	"bufio"
 )
 
-func main() {
-	p := pipeline.ArraySource(2, 3, 4, 5, 6, 1, 3, 6)
+func closeOff() {
+	fmt.Println("")
+	fmt.Println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+	fmt.Println()
+}
 
+func main() {
+
+
+}
+
+func testSort() {
+	p := pipeline.ArraySource(2, 3, 4, 5, 6, 1, 3, 6)
 	for {
 		// 从ArraySource的channel中取数据
 		if num, ok := <-p; ok {
-			fmt.Println(num)
+			fmt.Print(num)
+		} else {
+			break
+		}
+	}
+	closeOff()
+
+	pSort := pipeline.InMemorySort(pipeline.ArraySource(2, 3, 4, 5, 6, 1, 3, 6))
+	for {
+		// 从ArraySource的channel中取数据
+		if num, ok := <-pSort; ok {
+			fmt.Print(num)
 		} else {
 			break
 		}
 	}
 
+}
+
+func testMerge() {
+	closeOff()
+	merge2 := pipeline.Merge2(
+		pipeline.InMemorySort(pipeline.ArraySource(5, 6, 1, 3, 6)),
+		pipeline.InMemorySort(pipeline.ArraySource(2, 3, 4)))
+	for {
+		// 从ArraySource的channel中取数据
+		if num, ok := <-merge2; ok {
+			fmt.Print(num)
+		} else {
+			break
+		}
+	}
+}
+
+func testRand()  {
+	fmt.Println(rand.Int())
+	fmt.Println(rand.Uint64())
+	for i:=0;i<10;i++{
+		fmt.Println(rand.Intn(50))
+	}
+}
+
+func testFile(fileName string, size int) {
+	closeOff()
+
+	smallFile, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer smallFile.Close()
+	// 将file对象转为buff io,使其变为缓冲流，写入速度更快
+	writer  := bufio.NewWriter(smallFile)
+	// 生成随机数
+	data := pipeline.RandomIntSource(size)
+	// 写入文件
+	pipeline.WriterSink(writer, data)
+	// 清空缓存，避免数据不完整
+	writer.Flush()
+
+
+	// 读取文件数据
+	smallFile, err = os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	// 将file对象转为buff io,使其变为缓冲流，读取速度更快
+	reader  := bufio.NewReader(smallFile)
+	chData := pipeline.ReaderSource(reader)
+	// 排序
+	chData = pipeline.InMemorySort(chData)
+	for i := range chData {
+		fmt.Println(i)
+	}
 }
